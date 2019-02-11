b2a2d4d6fd4261ca3a8b5037fdbd952bbeff51ad
Bernhard Fluehmann <bernhard.fluehmann@gmail.com>
2019-02-12 00:00:22 +0100

    Code improvements, comments added

--- main.go
+++ main.go
@@ -13,8 +13,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"text/tabwriter"
+	"strings"
 
 	checker "github.com/BFLB/monitoringplugin"
 	r "github.com/BFLB/monitoringplugin/range"
@@ -23,75 +22,57 @@ import (
 	"github.com/BFLB/unifi"
 )
 
-const VERSION = "v0.1"
+const VERSION = "v0.2"
 
 var (
-	host          = flag.String("host", "", "Controller hostname")
-	port          = flag.String("port", "8443", "Controller port")
-	site          = flag.String("site", "default", "Site ID or name, UniFi v3 and later")
-	user          = flag.String("user", "", "Controller username")
-	pass          = flag.String("pass", "", "Controller password")
-	path          = flag.String("path", "", "Path of output file")
-	version       = flag.Int("version", 5, "Controller base version")
-	warning       = flag.String("warning", "7", "Execution Time(s) warning threshold")
-	critical      = flag.String("critical", "10", "Execution Time(s) critical threshold")
-	profileBlock  = flag.String("profileBlock", "", "If set, affected ports will be set to the given port profile, e.g. disabled")
-	profileCurr   = flag.String("profileCurr", "all", "Check only applies to ports of given port profile")
-	portNameBlock = flag.String("portNameBlock", "", "Set port name after blocking")
-	portNameCurr  = flag.String("portNameCurr", "", "Set port name after reset (set back to curr)")
-	v             = flag.Bool("V", false, "Version")
+	host              = flag.String("host", "", "Controller hostname")
+	port              = flag.String("port", "8443", "Controller port")
+	site              = flag.String("site", "default", "Site ID or name, UniFi v3 and later")
+	user              = flag.String("user", "", "Controller username")
+	pass              = flag.String("pass", "", "Controller password")
+	path              = flag.String("path", "", "Path of output file")
+	version           = flag.Int("version", 5, "Controller base version")
+	warning           = flag.String("warning", "7", "Execution Time(s) warning threshold")
+	critical          = flag.String("critical", "10", "Execution Time(s) critical threshold")
+	profileBlock      = flag.String("profileBlock", "", "If set, affected ports will be set to the given port profile, e.g. disabled")
+	profileCurr       = flag.String("profileCurr", "", "Check only applies to ports of given port profile. Only non default profiles allowed")
+	portNameBlock     = flag.String("portNameBlock", "", "Set port name after blocking")
+	portNameCurr      = flag.String("portNameCurr", "", "Set port name after reset (set back to curr)")
+	perfdata          = flag.Bool("perfdata", false, "Add perfdata")
+	eventFilterLimit  = flag.Int("eventFilterLimit", 3000, "Maximum  number of alert events to be fetched")
+	eventFilterStart  = flag.Int("eventFilterStart", 0, "At witch alarm event to start fetching")
+	eventFilterWithin = flag.Int("eventFilterWithin", 24, "How many hours back to be fetched")
+	v                 = flag.Bool("V", false, "Version")
 )
 
+// Counter struct used for extit message
 type counters struct {
-	events           int
-	archived         int
-	portsProvisioned int
-	portsBlocked     int
-	portsUnblocked   int
-	skipped          int
-	failed           int
+	events             int
+	provisionedBlock   int
+	provisionedUnblock int
+	blocked            int
+	unblocked          int
+	failed             int
 }
 
 func main() {
-	// TODO: Remove
-	w := new(tabwriter.Writer)
-	w.Init(os.Stdout, 0, 8, 3, ' ', 0)
-	defer w.Flush()
 
-	// counters (Used for check-result)
+	// Counters
 	counters := counters{}
 
-	// Create new check
+	// Create check
 	check := checker.New()
+
+	// Check-message
 	message := ""
 
 	// Create writer
 	writer := activeWriter.New()
 
-	// Fixme
-	// Print usage info (Override of flag.Usage)
-	/*flag.Usage = func() {
-		check.Message("Usage:")
-		check.Message("    host: Controller hostname (mandatory)")
-		check.Message("    port: Controller port (optional")
-		check.Message("    site: Site ID or name, UniFi v3 and later (optional)")
-		check.Message("    username: Controller username (mandatory)")
-		check.Message("    password: Controller password (mandatory)")
-		check.Message("    path: Path of output file (optional)")
-		check.Message("    version: Controller base version (Optional)")
-		check.Message("    warning: Execution Time(s) warning threshold (Optional)")
-		check.Message("    critical: Execution Time(s) critical threshold (Optional)")
-		check.Message("    currentProfile: Check only applies to ports of given port profile (Optional)")
-		check.Message("    blockProfile: If set, affected ports will be set to the given port profile, e.g. disabled (Optional)")
-		check.Message("    alias: Set port alias (optional)")
-		check.Message("    V: Version (optional)")
-		check.Status.Unknown()
-		writer.Write(check)
-	}*/
-
 	// Parse command-line args
 	flag.Parse()
 
+	// Version information
 	if *v {
 		message = fmt.Sprintf("Version: check=%s, monitoring-library:%s", VERSION, checker.VERSION)
 		check.Status.Unknown()
@@ -109,8 +90,16 @@ func main() {
 	if *pass == "" {
 		flag.Usage()
 	}
+	if *profileCurr == "" {
+		flag.Usage()
+	}
+	// Catch not allowed profile settings
+	switch strings.ToLower(*profileCurr) {
+	case "", "all", "disabled":
+		flag.Usage()
+	}
 
-	// Set ranges
+	// Set ranges (monitoring) TODO: Move down
 	var rangeExecWarn *r.Range
 	var rangeExecCrit *r.Range
 
@@ -133,13 +122,13 @@ func main() {
 	}
 	defer u.Logout()
 
-	// Select site
+	// Get site
 	site, err := u.Site(*site)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Get port-profiles
+	// Get current port-profile
 	currProfile, err := u.PortProfile(site, *profileCurr)
 	if err != nil {
 		message = fmt.Sprintf("Port-profile not found:%s", err.Error())
@@ -147,120 +136,47 @@ func main() {
 		check.Message(message)
 		writer.Write(check)
 	}
-	blockProfile, err := u.PortProfile(site, *profileBlock)
-	if err != nil {
-		message = fmt.Sprintf("Port-profile not found:%s", err.Error())
-		check.Status.Unknown()
-		check.Message(message)
-		writer.Write(check)
+
+	// Get block port-profile (optional)
+	var blockProfile *unifi.PortProfile
+	if *profileBlock == "" {
+		blockProfile = nil
+	} else {
+		blockProfile, err = u.PortProfile(site, *profileBlock)
+		if err != nil {
+			message = fmt.Sprintf("Port-profile not found:%s", err.Error())
+			check.Status.Unknown()
+			check.Message(message)
+			writer.Write(check)
+		}
 	}
 
 	// Set event filters
 	var eventFilter unifi.EventFilter
 	// TODO: Add flags
-	eventFilter.Limit = 3000
-	eventFilter.Start = 0
-	eventFilter.Within = 24
+	eventFilter.Limit = *eventFilterLimit
+	eventFilter.Start = *eventFilterStart
+	eventFilter.Within = *eventFilterWithin
 
 	// Get a slice of raw events
-	rawEvents, err := u.RawAlarms(site, eventFilter)
+	rawAlarms, err := u.RawAlarms(site, eventFilter)
 	if err != nil {
 		log.Fatalln(err)
 		return
 	}
 
 	// Get a slice of poe events
-	// TODO: Only events which are not acknowledged
-	var poeEvents []unifi.EVT_SW_PoeDisconnect
-	for _, rawEvent := range rawEvents {
+	events, err := poeEvents(rawAlarms)
 
-		switch rawEvent.Key {
-		case "EVT_SW_PoeDisconnect":
-
-			var e unifi.EVT_SW_PoeDisconnect
-			err := json.Unmarshal(rawEvent.Data, &e)
-			if err == nil {
-				// Avoid duplicates
-				if len(poeEvents) == 0 {
-					poeEvents = append(poeEvents, e)
-				} else {
-					var found bool
-					found = false
-					for _, poeEvent := range poeEvents {
-						if poeEvent.SwName == e.SwName && poeEvent.Port == e.Port {
-							found = true
-							// Override archived
-							if e.Archived == nil {
-								poeEvent.Archived = nil
-								break
-							} else if *e.Archived == false {
-								*poeEvent.Archived = false
-								break
-							}
-						}
-						// New event
-						if found == false {
-							poeEvents = append(poeEvents, e)
-						}
-					}
-				}
-			}
-		}
-	}
-
-	for _, poeEvent := range poeEvents {
-
-		// Is Alert Archived?
-		var archived bool
-		if poeEvent.Archived != nil {
-			if *poeEvent.Archived {
-				archived = true
-			} else {
-				archived = false
-			}
-		}
-
-		// Check if profile matches
-		match, err := matchProfile(u, site, poeEvent, currProfile, &counters)
+	for _, event := range events {
+		do(u, site, event, currProfile, blockProfile, *portNameCurr, *portNameBlock, &counters)
 		if err != nil {
 			counters.failed += 1
-		}
-		if match == false {
-			match, err = matchProfile(u, site, poeEvent, blockProfile, &counters)
-			if err != nil {
-				counters.failed += 1
-			}
-		}
-		if match == true {
-
-			//Check if port is already blocked
-			blocked, _ := portBlocked(u, site, poeEvent, blockProfile, &counters)
-
-			// If event not archived, block port
-			if archived == false {
-				fmt.Println("Archived false")
-				if blocked == false {
-					fmt.Println("Blocked false")
-					err = configurePort(u, site, poeEvent, currProfile, blockProfile, *portNameBlock, &counters)
-					if err != nil {
-						counters.failed += 1
-					}
-				}
-			} else {
-				fmt.Println("Archived true")
-				// Event archived. If port still blocked, unblock it
-				if blocked {
-					err = configurePort(u, site, poeEvent, blockProfile, currProfile, *portNameCurr, &counters)
-					if err != nil {
-						counters.failed += 1
-					}
-				}
-			}
 		}
 	}
 
 	// Everything done. Setup return values and quit
-	// Set status TODO: Comment
+	// Set status
 	status := status.New()
 	if counters.events > 0 {
 		status.Critical(false)
@@ -268,14 +184,15 @@ func main() {
 	if counters.failed > 0 {
 		status.Critical(false)
 	}
-	if counters.portsBlocked > 0 {
+	if counters.blocked > 0 {
 		status.Warning(false)
 	}
+	// TODO set status on execution time
 	status.Ok(false)
 	check.Status = status
 
 	// Add message
-	message = fmt.Sprintf("%d matching events, %d ports blocked (%d provisioned, %d failed)", counters.events, counters.portsBlocked, counters.portsProvisioned, counters.failed)
+	message = fmt.Sprintf("%d active matching alerts, %d ports blocked (%d provisioned-block, %d provisioned-unblock, %d failed)", counters.events, counters.blocked, counters.provisionedBlock, counters.provisionedUnblock, counters.failed)
 	check.Message(message)
 
 	// TODO Add performance data
@@ -285,7 +202,9 @@ func main() {
 
 }
 
-func configurePort(u *unifi.Unifi, site *unifi.Site, event unifi.EVT_SW_PoeDisconnect, currProfile *unifi.PortProfile, newProfile *unifi.PortProfile, newPortName string, c *counters) error {
+// Gets the switch and port and checks if the configured profile belongs to the check.
+// Adds coniguration changes to the port depending of command arguments
+func do(u *unifi.Unifi, site *unifi.Site, event unifi.EVT_SW_PoeDisconnect, currProfile *unifi.PortProfile, blockProfile *unifi.PortProfile, currPortName string, blockPortName string, c *counters) error {
 
 	// Get the switch
 	usw, err := u.USW(site, event.SwName)
@@ -296,74 +215,98 @@ func configurePort(u *unifi.Unifi, site *unifi.Site, event unifi.EVT_SW_PoeDisco
 	// Get port-overrides (non default settings)
 	overrides := usw.PortOverrides
 
-	// Get the port
+	// Find the port
 	for i := range usw.PortOverrides {
 		if overrides[i].PortIdx == event.Port {
-			// Check if belongs to current profile
-			if overrides[i].PortconfID == currProfile.ID {
-
-				// If new profile available, configure it
-				if newProfile != nil {
-					overrides[i].PortconfID = newProfile.ID
-					c.portsProvisioned += 1
-
-					// Set the new description
-					if newPortName != "" {
-						overrides[i].Name = newPortName
+			// Active (non archived) event
+			if archived(event) == false {
+				// Check if port belongs current profile matches
+				if overrides[i].PortconfID == currProfile.ID {
+					c.events += 1
+					// Check if must be blocked
+					if blockProfile != nil {
+						// Change settings (block)
+						overrides[i].PortconfID = blockProfile.ID
+						if blockPortName != "" {
+							overrides[i].Name = blockPortName
+						}
+						u.SetPortoverrides(site, usw.DeviceID, overrides)
+						c.provisionedBlock += 1
 					}
-					u.SetPortoverrides(site, usw.DeviceID, overrides)
+				} else { // Check if port already blocked
+					if overrides[i].PortconfID == blockProfile.ID {
+						c.events += 1
+						c.blocked += 1
+					}
 				}
-				return nil
+			} else { // Archived event
+				// Check if port belongs to blocked profile
+				if blockProfile != nil {
+					if overrides[i].PortconfID == blockProfile.ID {
+						c.blocked += 1
+						// Change settings (unblock)
+						overrides[i].PortconfID = currProfile.ID
+						if currPortName != "" {
+							overrides[i].Name = currPortName
+						}
+						u.SetPortoverrides(site, usw.DeviceID, overrides)
+						c.provisionedUnblock += 1
+					}
+				}
 			}
+			break
 		}
 	}
 	return nil
 }
 
-func portBlocked(u *unifi.Unifi, site *unifi.Site, event unifi.EVT_SW_PoeDisconnect, blockProfile *unifi.PortProfile, c *counters) (blocked bool, err error) {
+// Returns a slice with poeEvents. One per switch/port combination. Non-Archived wins.
+func poeEvents(rawEvents []unifi.RawAlarm) ([]unifi.EVT_SW_PoeDisconnect, error) {
+	var poeEvents []unifi.EVT_SW_PoeDisconnect
+	for _, rawEvent := range rawEvents {
 
-	// Get the switch
-	usw, err := u.USW(site, event.SwName)
-	if err != nil {
-		return false, err
-	}
+		switch rawEvent.Key {
+		case "EVT_SW_PoeDisconnect":
+			// Unmarshal rawEvent to poeEvent
+			var e unifi.EVT_SW_PoeDisconnect
+			err := json.Unmarshal(rawEvent.Data, &e)
+			if err != nil {
+				return poeEvents, err
+			}
 
-	// Get the port
-	overrides := usw.PortOverrides
-
-	// Get the port
-	for i := range usw.PortOverrides {
-		if overrides[i].PortIdx == event.Port {
-			// Port already blocked.
-			if overrides[i].PortconfID == blockProfile.ID {
-				c.portsBlocked += 1
-				return true, nil
+			// Avoid duplicates
+			if len(poeEvents) == 0 {
+				poeEvents = append(poeEvents, e)
+			} else {
+				var found bool
+				found = false
+				for _, poeEvent := range poeEvents {
+					if poeEvent.SwName == e.SwName && poeEvent.Port == e.Port {
+						found = true
+						// Override archived
+						if e.Archived == nil {
+							poeEvent.Archived = nil
+							break
+						} else if *e.Archived == false {
+							*poeEvent.Archived = false
+							break
+						}
+					}
+				}
+				// New event
+				if found == false {
+					poeEvents = append(poeEvents, e)
+				}
 			}
 		}
 	}
-	return false, nil
+	return poeEvents, nil
 }
 
-func matchProfile(u *unifi.Unifi, site *unifi.Site, event unifi.EVT_SW_PoeDisconnect, currProfile *unifi.PortProfile, c *counters) (match bool, err error) {
-
-	// Get the switch
-	usw, err := u.USW(site, event.SwName)
-	if err != nil {
-		return false, err
+// Check if event is archived
+func archived(event unifi.EVT_SW_PoeDisconnect) bool {
+	if event.Archived == nil {
+		return false
 	}
-
-	// Get the port
-	overrides := usw.PortOverrides
-
-	// Get the port
-	for i := range usw.PortOverrides {
-		if overrides[i].PortIdx == event.Port {
-			// Match
-			if overrides[i].PortconfID == currProfile.ID {
-				c.events += 1
-				return true, nil
-			}
-		}
-	}
-	return false, nil
+	return *event.Archived
 }
